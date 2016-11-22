ea6b31fd2ba35b3222373f447b901408d2c24dca
Staszek1903 <staszek197@op.pl>
2016-11-22 23:15:05 +0100

    nwm co sie dzieje

---
+++ src/event_rec.cpp
@@ -0,0 +1,31 @@
+#include "event_rec.h"
+
+Event_rec::Event_rec()
+{
+    //ctor
+}
+
+Event_rec::~Event_rec()
+{
+    //dtor
+}
+
+void Event_rec::set_speed_ref(std::pair <float, float> &ds)
+{
+    dir_speed = &ds;
+}
+
+void Event_rec::rec_event_list(std::vector <Event> ev_list)
+{
+    for(unsigned int i=0; i <ev_list.size(); i++)
+    {
+        switch(ev_list[i].get_type())
+        {
+        case Event::SOLID:
+            dir_speed->first = 0;
+            dir_speed->second = 0;
+            std::cout<<"SOLID KURWA"<<std::endl;
+        break;
+        }
+    }
+}
